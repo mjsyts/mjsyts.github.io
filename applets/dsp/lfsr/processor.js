baec339668bb0b15517d7608e7351e07b45dcc81
Josiah Sytsma <77947318+mjsyts@users.noreply.github.com>
2026-01-03 00:08:33 -0500

    Update processor.js

--- applets/dsp/lfsr/processor.js
+++ applets/dsp/lfsr/processor.js
@@ -1,35 +1,41 @@
 class LfsrNoiseProcessor extends AudioWorkletProcessor {
-    static get parameterDescriptors() {
-        return [{
-            name: 'amplitude',
-            defaultValue: 0.10,
-            minValue: 0.0,
-            maxValue: 1.0,
-            automationRate: 'a-rate'
-        }];
+  static get parameterDescriptors() {
+    return [{
+      name: 'amplitude',
+      defaultValue: 0.10,
+      minValue: 0.0,
+      maxValue: 1.0,
+      automationRate: 'a-rate'
+    }];
+  }
+
+  constructor() {
+    super();
+    this.state = 0x7fff;
+  }
+
+  process(inputs, outputs, parameters) {
+    const out = outputs[0];
+    const ampParam = parameters.amplitude;
+
+    for (let ch = 0; ch < out.length; ch++) {
+      const channel = out[ch];
+
+      for (let i = 0; i < channel.length; i++) {
+        const lsb0 = this.state & 1;
+        const lsb1 = (this.state >> 1) & 1;
+        const fb = lsb0 ^ lsb1;
+
+        this.state = (this.state >> 1) | (fb << 14);
+
+        const sample = lsb0 ? 1.0 : -1.0;
+        const amp = (ampParam.length === 1) ? ampParam[0] : ampParam[i];
+
+        channel[i] = sample * amp;
+      }
     }
-
-    constructor() {
-        super();
-        this.state = 0x7fff;
-    }
-
-    process(inputs, outputs, parameters) {
-        const out = outputs[0];
-        const ampParam = parameters.amplitude;
-
-        for (let ch = 0; ch < out.length; ch++) {
-            for (let i = 0; i < channel.length; i++) {
-                const lsb0 = this.state & 1;
-                const lsb1 = (this.state >> 1) & 1;
-                const fb = lsb0 ^ lsb1;
-                this.state = (this.state >> 1) | (fb << 14);
-
-                const sample = lsb0 ? 1.0 : -1.0;
-                const amp = ampParam.length === 1 ? ampParam[0] : ampParam[i];
-                channel[i] = sample * amp;
-            }
-        }
-        return true;
-    }
+    return true;
+  }
 }
+
+registerProcessor('lfsr-noise', LfsrNoiseProcessor);
